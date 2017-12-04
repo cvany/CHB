9412a2efe60f157741302095391cde9f5a0c7ee3
shilim <1251314045@qq.com>
2017-12-04 09:50:30 +0800

    完成定位页面

---
+++ WebRoot/js/controller/front/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by shilim on 2017/12/1.
+ */
+/**
+ * 菜单控制器
+ */
+var controllers = angular.module("controllers", []);
+
+/**
+ * 主页控制器
+ */
+controllers.controller("index",['$scope',function($scope) {
+
+
+}]);
---
+++ WebRoot/js/controller/front/indexApp.js
@@ -0,0 +1,22 @@
+var indexApp = angular.module("indexApp",['ui.router','controllers']);
+var baseUrl = "/CHB/";
+indexApp.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider) {
+	$urlRouterProvider.when("","/location");
+	$stateProvider
+	.state("location",{
+		url:"/location",
+		templateUrl:"location/location.html"
+	})
+}])
+.config(['$httpProvider',function($httpProvider) {
+	$httpProvider.defaults.transformRequest=function(obj){
+		var str=[];
+		for(var p in obj){
+			str.push(encodeURIComponent(p)+"="+encodeURIComponent(obj[p]));
+		}
+		return str.join("&");
+	};
+	$httpProvider.defaults.headers.post = {
+			'Content-Type': 'application/x-www-form-urlencoded',
+	}
+}]);
