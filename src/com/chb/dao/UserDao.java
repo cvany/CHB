9c0068e44699edb77c775afa5ccd4f8bcf590086
vany <814675492@qq.com>
2017-12-04 10:42:14 +0800

    修改编码格式为UTF8

--- src/com/chb/dao/UserDao.java
+++ src/com/chb/dao/UserDao.java
@@ -3,7 +3,11 @@ package com.chb.dao;
 import org.springframework.stereotype.Repository;
 
 import com.chb.entity.User;
-
+/**
+ * 用户dao层接口
+ * @author 崔文元
+ *
+ */
 @Repository("userDao")
 public interface UserDao {
 	User findByUsername(String username);
