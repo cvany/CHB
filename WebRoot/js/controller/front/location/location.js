3fb72a5fe5ab105e330003ce3371b129449d1364
vany <814675492@qq.com>
2017-12-06 08:45:22 +0800

    修改商家实体类phone字段为string类型 完成定位

--- WebRoot/js/controller/front/location/location.js
+++ WebRoot/js/controller/front/location/location.js
@@ -20,7 +20,14 @@ controllers.controller("location",['$scope','$timeout',function($scope,$timeout)
 		// 将地址解析结果显示在地图上,并调整地图视野
 		myGeo.getPoint(val, function(point){
 			if (point) {
-	          	alert("经度："+point.lng+"纬度："+point.lat);
+//	          	alert("经度："+point.lng+"纬度："+point.lat);
+	         // 百度地图API功能
+	        	var map = new BMap.Map("allmap");            
+	        	map.centerAndZoom(new BMap.Point(point.lng,point.lat), 15);  
+	        	var bs = map.getBounds();   //获取可视区域
+	        	var bssw = bs.getSouthWest();   //可视区域左下角
+	        	var bsne = bs.getNorthEast();   //可视区域右上角
+	        	alert("当前地图可视范围是：" + bssw.lng + "," + bssw.lat + "到" + bsne.lng + "," + bsne.lat);
 			}else{
 				alert("您选择地址没有解析到结果!");
 			}
